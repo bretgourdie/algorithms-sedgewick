3b3be95da6a8d82711f0c8fa2f79316de6a04150
Bret Gourdie <BretGourdie@gmail.com>
2017-04-02 22:21:11 -0400

    Added Section01 exercises

---
+++ Chapter01/Section01.java
@@ -0,0 +1,22 @@
+public class Section01
+{
+    public static void main(String[] args)
+    {
+	Ex1x1x1();
+    }
+
+    public static void Ex1x1x1()
+    {
+	System.out.println(
+	    "a) ( 0 + 15 ) / 2 = "
+	    + String.valueOf(( 0 + 15 ) / 2));
+
+	System.out.println(
+	    "b) 2.0e-6 * 100000000.1 = "
+	    + String.valueOf(2.0e-6 * 100000000.1));
+	
+	System.out.println(
+	    "c) true && false || true && true = "
+	    + String.valueOf(true && false || true && true));
+    }
+}
