b531ebeb97daed483fe6c16a5ea7abe3f6b3908d
Bret Gourdie <BretGourdie@gmail.com>
2017-04-02 22:37:49 -0400

    Ex 1.1.2

--- Chapter01/Section01.java
+++ Chapter01/Section01.java
@@ -3,20 +3,46 @@ public class Section01
     public static void main(String[] args)
     {
 	Ex1x1x1();
+	Ex1x1x2();
     }
 
     public static void Ex1x1x1()
     {
+	System.out.println("Ex 1.1.1:");
 	System.out.println(
-	    "a) ( 0 + 15 ) / 2 = "
+	    "\ta) ( 0 + 15 ) / 2 = "
 	    + String.valueOf(( 0 + 15 ) / 2));
 
 	System.out.println(
-	    "b) 2.0e-6 * 100000000.1 = "
+	    "\tb) 2.0e-6 * 100000000.1 = "
 	    + String.valueOf(2.0e-6 * 100000000.1));
 	
 	System.out.println(
-	    "c) true && false || true && true = "
+	    "\tc) true && false || true && true = "
 	    + String.valueOf(true && false || true && true));
+    }
+
+    public static void Ex1x1x2()
+    {
+	System.out.println("Ex 1.1.2:");
+	System.out.println(
+	    "\ta) (1 + 2.236)/2 = "
+	    + String.valueOf((1 + 2.236)/2)
+	    + " (double)");
+
+	System.out.println(
+	    "\tb) 1 + 2 + 3 + 4.0 = "
+	    + String.valueOf(1 + 2 + 3 + 4.0)
+	    + " (double)");
+	
+	System.out.println(
+	    "\tc) 4.1 >= 4 = "
+	    + String.valueOf(4.1 >= 4)
+	    + " (boolean)");
+
+	System.out.println(
+	    "\td) 1 + 2 + \"3\" = "
+	    + String.valueOf(1 + 2 + "3")
+	    + " (String)");
     }
 }
