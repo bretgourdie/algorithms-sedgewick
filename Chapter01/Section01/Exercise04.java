c0d17b8154074248ea0a62719494335ddc4ab318
Bret Gourdie <BretGourdie@gmail.com>
2017-04-02 23:15:07 -0400

    Solved 1.1.4

---
+++ Chapter01/Section01/Exercise04.java
@@ -0,0 +1,19 @@
+public class Exercise04
+{
+    public static void main(String[] args)
+    {
+	System.out.println("Ex 1.1.4:");
+
+	System.out.println(
+	    "\ta) \"then\" is not a Java keyword");
+
+	System.out.println(
+	    "\tb) \"if\" statements require parenthesis");
+
+	System.out.println(
+	    "\tc) Code is valid");
+	
+	System.out.println(
+	    "\td) \"c = 0\" is missing a semicolon");
+    }
+}
