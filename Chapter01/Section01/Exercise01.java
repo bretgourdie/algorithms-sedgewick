6ce0a52a521fc0c1934a59773a29f3cf0e19acb1
Bret Gourdie <BretGourdie@gmail.com>
2017-04-02 22:48:13 -0400

    Split Exercises to their own classes

---
+++ Chapter01/Section01/Exercise01.java
@@ -0,0 +1,18 @@
+public class Exercise01
+{
+    public static void main(String[] args)
+    {
+	System.out.println("Ex 1.1.1:");
+	System.out.println(
+	    "\ta) ( 0 + 15 ) / 2 = "
+	    + String.valueOf(( 0 + 15 ) / 2));
+
+	System.out.println(
+	    "\tb) 2.0e-6 * 100000000.1 = "
+	    + String.valueOf(2.0e-6 * 100000000.1));
+
+	System.out.println(
+	    "\tc) true && false || true && true = "
+	    + String.valueOf(true && false || true && true));
+    }
+}
