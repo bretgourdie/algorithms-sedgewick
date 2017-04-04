a77ac9f41840eadb1e23f09904fe3aed6e2ec4b3
Bret Gourdie <bretgourdie@gmail.com>
2017-04-04 09:36:50 -0400

    Solved 1.1.6

--- Chapter01/Section01/Exercise06.java
+++ Chapter01/Section01/Exercise06.java
@@ -6,6 +6,9 @@ public class Exercise06
 {
     public static void main(String[] args)
     {
+        System.out.println(
+            "Prints the first 16 digits of the Fibonacci sequence:\n"
+            + "\t 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610");
 	int f = 0;
 	int g = 1;
 	for (int i = 0; i <= 15; i++)
