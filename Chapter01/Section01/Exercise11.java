1171c94d175a53c44f4c06fb1a24f00ebb347212
Bret Gourdie <bretgourdie@gmail.com>
2017-04-04 14:34:11 -0400

    Added column headers

--- Chapter01/Section01/Exercise11.java
+++ Chapter01/Section01/Exercise11.java
@@ -15,15 +15,48 @@ public class Exercise11
 
     public static void print2dBoolArray(boolean[][] array)
     {
+        int max = getMax(array);
+        String formatText = getFormatText(max);
+
+        printColumnNumbers(max, formatText);
+
         for(int i = 0; i < array.length; i++)
         {
             for(int j = 0; j < array[i].length; j++)
             {
                 String toPrint = array[i][j] ? "*" : " ";
-                System.out.print(toPrint);
+                System.out.printf(formatText, toPrint);
             }
 
             System.out.println();
         }
+    }
+
+    public static void printColumnNumbers(int max, String formatText)
+    {
+        for(int i = 0; i < max; i++)
+        {
+            System.out.printf(formatText, i+1);
+        }
+        System.out.println();
+    }
+
+    public static int getMax(boolean[][] array)
+    {
+        int max = 0;
+
+        for(int i = 0; i < array.length; i++)
+        {
+            max = Math.max(max, array[i].length);
+        }
+
+        return max;
+    }
+
+    public static String getFormatText(int max)
+    {
+        int numDigits = String.valueOf(max).length();
+        String formatText = "%1$" + numDigits + "s ";
+        return formatText;
     }
 }
