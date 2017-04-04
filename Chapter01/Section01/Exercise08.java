5a605f293b6fd87222e9d60c4ce3bf456155a011
Bret Gourdie <bretgourdie@gmail.com>
2017-04-04 13:51:40 -0400

    Added explanations as the problems asked for

--- Chapter01/Section01/Exercise08.java
+++ Chapter01/Section01/Exercise08.java
@@ -11,21 +11,21 @@ public class Exercise08
 
     public static void a()
     {
-        System.out.println("a) ");
+        System.out.println("a) Prints the letter 'b':");
 
         System.out.println('b');
     }
 
     public static void b()
     {
-        System.out.println("b) ");
+        System.out.println("b) Prints the result of the ASCII values of 'b' and 'c' summed together:");
 
         System.out.println('b' + 'c');
     }
 
     public static void c()
     {
-        System.out.println("c) ");
+        System.out.println("c) Prints the ASCII character four after 'a':");
 
         System.out.println((char) ('a' + 4));
     }
