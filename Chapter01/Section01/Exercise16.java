3dd15ebbb9f60d1740b45d61c58aa7e41d3b77ff
Bret Gourdie <bretgourdie@gmail.com>
2017-04-05 09:27:40 -0400

    Solved 1.1.16

---
+++ Chapter01/Section01/Exercise16.java
@@ -0,0 +1,15 @@
+package Chapter01.Section01;
+
+public class Exercise16
+{
+    public static void main(String[] args)
+    {
+        System.out.println(exR1(6));
+    }
+
+    public static String exR1(int n)
+    {
+        if (n <= 0) return "";
+        return exR1(n-3) + n + exR1(n-2) + n;
+    }
+}
