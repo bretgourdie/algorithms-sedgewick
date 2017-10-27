aa09c85a21d3bd2a86168b318647e2fcf5157b45
Bret Gourdie <BretGourdie@gmail.com>
2017-10-27 16:30:21 -0400

    Solved 1.3.3

---
+++ Chapter1/Section3/Exercise03.java
@@ -0,0 +1,18 @@
+package Chapter1.Section3;
+
+import edu.princeton.cs.algs4.*;
+
+public class Exercise03
+{
+    public static void main (String[] args)
+    {
+	StdOut.println("a) 4 3 2 1 0 9 8 7 6 5: can!");
+	StdOut.println("b) 4 6 8 7 5 3 2 9 0 1: can't pop 0 before 1");
+	StdOut.println("c) 2 5 6 7 4 8 9 3 1 0: can!");
+	StdOut.println("d) 4 3 2 1 0 5 6 7 8 9: can!");
+	StdOut.println("e) 1 2 3 4 5 6 9 8 7 0: can!");
+	StdOut.println("f) 0 4 6 5 3 8 1 7 2 9: can't pop 1 before 2");
+	StdOut.println("g) 1 4 7 9 8 6 5 3 0 2: can't pop 0 before 2");
+	StdOut.println("h) 2 1 4 3 6 5 8 7 9 0: can!");
+    }
+}
