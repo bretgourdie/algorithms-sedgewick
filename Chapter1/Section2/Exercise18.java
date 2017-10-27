99715bda311e269f694512dd59010d4272ef8421
Bret Gourdie <BretGourdie@gmail.com>
2017-10-26 22:49:48 -0400

    Solved(?) 1.2.28

---
+++ Chapter1/Section2/Exercise18.java
@@ -0,0 +1,11 @@
+package Chapter1.Section2;
+
+import edu.princeton.cs.algs4.*;
+
+public class Exercise18
+{
+    public static void main (String[] args)
+    {
+	StdOut.println("This code is valid; the int is cast to a double to keep precision correct.");
+    }
+}
