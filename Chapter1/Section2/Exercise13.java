28e4febaa9c4e57b91b599ff230e0bcb315a8d82
Bret Gourdie <BretGourdie@gmail.com>
2017-10-26 23:02:32 -0400

    Rework new definition of Transaction

--- Chapter1/Section2/Exercise13.java
+++ Chapter1/Section2/Exercise13.java
@@ -6,8 +6,8 @@ public class Exercise13
 {
     public static void main (String[] args)
     {
-	Transaction deposit = new Transaction(10, 26, 2017, 150);
-	Transaction withdrawal = new Transaction(10, 26, 2017, -70);
+	Transaction deposit = new Transaction("The Techs", 10, 26, 2017, 150);
+	Transaction withdrawal = new Transaction("SDI Butler", 10, 26, 2017, -70);
 
 	StdOut.println(deposit + "\n" + withdrawal);
     }
--- Chapter1/Section2/Exercise14.java
+++ Chapter1/Section2/Exercise14.java
@@ -15,7 +15,7 @@ public class Exercise14
     public static void testDifferentClass()
     {
 	StdOut.println("testDifferentClass()");
-	Transaction t = new Transaction(1, 1, 2017, 42);
+	Transaction t = new Transaction("Customer", 1, 1, 2017, 42);
 	Counter c = new Counter("transaction?");
 
 	if (!t.equals(c))
@@ -25,7 +25,7 @@ public class Exercise14
     public static void testSameTrans()
     {
 	StdOut.println("testSameTrans()");
-	Transaction one = new Transaction(1, 1, 2017, 42);
+	Transaction one = new Transaction("Customer", 1, 1, 2017, 42);
 	Transaction alsoOne = one;
 
 	if (one.equals(alsoOne))
@@ -35,8 +35,8 @@ public class Exercise14
     public static void testNotEqualTrans()
     {
 	StdOut.println("testNotEqualTrans()");
-	Transaction one = new Transaction(1, 1, 2017, 42);
-	Transaction notOne = new Transaction(4, 5, 2016, 50);
+	Transaction one = new Transaction("Customer", 1, 1, 2017, 42);
+	Transaction notOne = new Transaction("Buyer", 4, 5, 2016, 50);
 
 	if (!one.equals(notOne))
 	    pGood();
@@ -52,8 +52,9 @@ public class Exercise14
 	StdOut.println("testEqualTrans()");
 
 	int month = 1, day = 1, year = 2017, amount = 42;
-	Transaction one = new Transaction(month, day, year, amount);
-	Transaction two = new Transaction(month, day, year, amount);
+	String customer = "Some customer";
+	Transaction one = new Transaction(new String(customer), month, day, year, amount);
+	Transaction two = new Transaction(new String(customer), month, day, year, amount);
 
 	if (one.equals(two))
 	    pGood();
