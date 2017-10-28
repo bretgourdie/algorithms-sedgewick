ccd3bf8a5c03113232cb0bede49074695888bbdc
Bret Gourdie <BretGourdie@gmail.com>
2017-10-27 20:42:51 -0400

    Solved(?) 1.3.8

---
+++ Chapter1/Section3/Exercise08.java
@@ -0,0 +1,12 @@
+package Chapter1.Section3;
+
+import edu.princeton.cs.algs4.*;
+
+public class Exercise08
+{
+    public static void main (String[] args)
+    {
+	StdOut.println("Contents: \"it\"");
+	StdOut.println("Size: max of 5 items; probably 10?");
+    }
+}
