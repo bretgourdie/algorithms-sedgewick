7b038b58c4c0c6b5f7b27e13460a849bc7a17bbc
Bret Gourdie <BretGourdie@gmail.com>
2017-10-28 13:35:37 -0400

    Solved(?) 1.3.13

---
+++ Chapter1/Section3/Exercise13.java
@@ -0,0 +1,14 @@
+package Chapter1.Section3;
+
+import edu.princeton.cs.algs4.*;
+
+public class Exercise13
+{
+    public static void main(String[] args)
+    {
+	StdOut.println("a) 0 1 2 3 4 5 6 7 8 9: Good!");
+	StdOut.println("b) 4 6 8 7 5 3 2 9 0 1: Can't print 4 before 0");
+	StdOut.println("c) 2 5 6 7 4 8 9 3 1 0: Can't print 2 before 0");
+	StdOut.println("d) 4 3 2 1 0 5 6 7 8 9: Can't print 4 before 0");
+    }
+}
