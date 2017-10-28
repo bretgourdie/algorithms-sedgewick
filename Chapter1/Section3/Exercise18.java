4fe2a870c9ec20310def78be7542b36b5ac3db50
Bret Gourdie <BretGourdie@gmail.com>
2017-10-28 16:10:08 -0400

    Solved 1.3.18

---
+++ Chapter1/Section3/Exercise18.java
@@ -0,0 +1,12 @@
+package Chapter1.Section3;
+
+import edu.princeton.cs.algs4.*;
+
+public class Exercise18
+{
+    public static void main (String[] args)
+    {
+	StdOut.println("Code: x.next = x.next.next;");
+	StdOut.println("Explanation: x.next is set to x.next's next, effectively removing the previous x.next from the list.");
+    }
+}
